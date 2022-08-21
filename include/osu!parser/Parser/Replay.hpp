f119527a72f13163ab878c88dfbce73b55628d65
JustCabbage <arjunjuneja4u@gmail.com>
2022-08-21 04:38:49 +0100

    Update README | Replay Parser Boilerplate

--- include/osu!parser/Parser/Replay.hpp
+++ include/osu!parser/Parser/Replay.hpp
@@ -1,1 +1,25 @@
 #pragma once
+#include <string>
+#include <fstream>
+
+namespace Parser
+{
+    class Replay
+    {
+    public:
+        Replay(const std::string& ReplayPath) : m_CurrentStream(ReplayPath)
+        {
+            this->Reset();
+            if(!m_CurrentStream.good())
+            {
+                return;
+            }
+        }
+    private:
+        void Reset() {}
+    public:
+        // Publically Accessible Fields
+    private:
+        std::ifstream m_CurrentStream;
+    };
+}
